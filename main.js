19c7050294dee095915e5984c1ef912a0503e47b
Harkishen-Singh <harkishensingh@hotmail.com>
2018-05-24 18:57:21 +0530

    continue 24th of may

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function start(){
 		
 	}
 	default_lining();
+	control_figures();
 }
 
 function default_lining(argument) {
@@ -27,4 +28,25 @@ function default_lining(argument) {
 	svg.append('line').attr('x1', 0).attr('y1', d_length).attr('x2',d_breadth*3).attr('y2',d_length).style('stroke','black');
 	svg.append('line').attr('x1', 0).attr('y1', d_length*2).attr('x2',d_breadth*3).attr('y2',d_length*2).style('stroke','black');
 }
+function control_figures(argument) {
+	var x_cordinate_line1 = 5; // 5 pixel from left margin
+	var y_cordinate_line1 = 5; // 5 pixel from the top margin
+	var radi = 70, circle_x = 200, circle_y = 200 ;
+
+	var x_travel_line = 100, y_travel_line = 100;
+
+	var x_cordinate_line2 = 5;
+	var y_cordinate_line2 = 5+x_travel_line;
+	// making of the lines
+
+	grp.append('line').attr('x1', x_cordinate_line1).attr('y1', y_cordinate_line1).attr('x2',x_cordinate_line1+x_travel_line )
+	.attr('y2', y_cordinate_line1+y_travel_line).style('stroke', 'black').attr('stroke-width','2');
+	grp.append('line').attr('x1', x_cordinate_line2).attr('y1', y_cordinate_line2).attr('x2',x_cordinate_line2+x_travel_line )
+	.attr('y2', y_cordinate_line2-y_travel_line).style('stroke', 'black').attr('stroke-width','2');
+
+	grp.append('circle').attr('r', radi).attr('cx',circle_x).attr('cy',circle_y).attr('fill','none')
+		.attr('stroke', 'black').attr('stroke-width','2') ;
+	
+}
+
 start();
--- test.js
+++ test.js
@@ -3,6 +3,7 @@ var grp = svg.append('g').attr('transform', 'translate(0,0) ');
 
 var x_cordinate_line1 = 5; // 5 pixel from left margin
 var y_cordinate_line1 = 5; // 5 pixel from the top margin
+var radi = 70, circle_x = 200, circle_y = 200 ;
 
 var x_travel_line = 100, y_travel_line = 100;
 
@@ -11,8 +12,10 @@ var y_cordinate_line2 = 5+x_travel_line;
 // making of the lines
 
 grp.append('line').attr('x1', x_cordinate_line1).attr('y1', y_cordinate_line1).attr('x2',x_cordinate_line1+x_travel_line )
-.attr('y2', y_cordinate_line1+y_travel_line).style('stroke', 'black');
+.attr('y2', y_cordinate_line1+y_travel_line).style('stroke', 'black').attr('stroke-width','2');
 grp.append('line').attr('x1', x_cordinate_line2).attr('y1', y_cordinate_line2).attr('x2',x_cordinate_line2+x_travel_line )
-.attr('y2', y_cordinate_line2-y_travel_line).style('stroke', 'black');
+.attr('y2', y_cordinate_line2-y_travel_line).style('stroke', 'black').attr('stroke-width','2');
 
+grp.append('circle').attr('r', radi).attr('cx',circle_x).attr('cy',circle_y).attr('fill','none')
+	.attr('stroke', 'black').attr('stroke-width','2') ;
 	
